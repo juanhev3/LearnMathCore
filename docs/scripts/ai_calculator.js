9a363484709f040dde5f533cd79efaf9f2d9490d
juanhev3 <65797398+juanhev3@users.noreply.github.com>
2025-06-30 19:32:30 -0700

    Update ai_calculator.js

--- docs/scripts/ai_calculator.js
+++ docs/scripts/ai_calculator.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const expression = inputArea.value;
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/calculate", {
+      const response = await fetch("http://learnmathcore-backend.onrender.com/calculate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
